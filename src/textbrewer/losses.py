e3d5cc6f83d71b95022f32d99ba57047fbf0aa5e
Boris Bubla <56068500+bobub@users.noreply.github.com>
2021-05-19 10:26:30 +0200

    Update losses.py

--- src/textbrewer/losses.py
+++ src/textbrewer/losses.py
@@ -104,7 +104,11 @@ def att_ce_loss(attention_S, attention_T, mask=None):
         loss = -((probs_T_select * F.log_softmax(attention_S, dim=-1)).sum(dim=-1)).mean()
     else:
         #print('Probs_T: ', probs_T.shape, '\n', probs_T)
-        mask = mask.to(attention_S).unsqueeze(1).expand(-1, attention_S.size(1), -1) # (bs, num_of_heads, len)
+        mask = mask.to(attention_S).unsqueeze(1).expand(-1, attention_S.size(1), -1)
+        assert torch.any(torch.isnan(probs_T_select))==False, 'Att_CE Probs_T_select is NaN'
+        assert torch.any(torch.isnan(F.log_softmax(attention_S, dim=-1)))==False, 'log_softmax of attention_S is NaN'
+        assert torch.any(torch.isnan((probs_T_select * F.log_softmax(attention_S, dim=-1))))==False, 'product of log_softmax and probs_T_select is NaN'
+        # (bs, num_of_heads, len)
         #print('mask: ', mask.shape, '\n', mask)
         #print('probs_T*log_softmax(att_S): ', (probs_T * F.log_softmax(attention_S, dim=-1) * mask.unsqueeze(2)).sum(dim=-1).shape, "\n", (probs_T * F.log_softmax(attention_S, dim=-1) * mask.unsqueeze(2)).sum(dim=-1))
         loss = -((probs_T * F.log_softmax(attention_S, dim=-1) * mask.unsqueeze(2)).sum(dim=-1) * mask).sum() / mask.sum()
