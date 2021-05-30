7cb3b497b1a046cd39ef3ebd9d04973c0c3c2af1
Boris Bubla <56068500+bobub@users.noreply.github.com>
2021-05-30 11:01:39 +0200

    Update distiller_general.py

--- src/textbrewer/distiller_general.py
+++ src/textbrewer/distiller_general.py
@@ -173,10 +173,12 @@ class GeneralDistiller(BasicDistiller):
                     #inter_T = [self.projs[ith](t) for t in inter_T]
                     inter_S = [self.projs[ith](s) for s in inter_S]
             else:
-                #print('Inters_S:',inters_S)
-                #print('Inters_T:',inters_T)
+                print('Inters_S:',inters_S)
+                print('Inters_T:',inters_T)
                 inter_S = inters_S[feature][layer_S]
                 inter_T = inters_T[feature][layer_T]
+                print('Inter_S: ',inter_S)
+                print('Inter_T: ',inter_T)
                 name_S = str(layer_S)
                 name_T = str(layer_T)
                 if self.projs[ith]:
